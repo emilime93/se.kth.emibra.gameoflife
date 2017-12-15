cc7d73c1db5d7156f429e32bf1d58cefe92429dc
Emil <emil93@gmail.com>
2017-12-16 00:14:27 +0100

    Game of Life Finnished!

--- canvas.js
+++ canvas.js
@@ -2,8 +2,8 @@ var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
 var cellSize = 12;
-canvas.width = window.innerWidth - 30;
-canvas.height = window.innerHeight - 30;
+canvas.width = window.innerWidth - 6;
+canvas.height = window.innerHeight - 6;
 
 var numCellsX = parseInt(canvas.width / cellSize);
 var numCellsY = parseInt(canvas.height / cellSize);
@@ -12,6 +12,7 @@ console.log(numCellsX);
 console.log(numCellsY);
 
 var dataGrid = new Array(numCellsX);
+var previousGeneration;
 
 function setup() {
     
@@ -47,9 +48,76 @@ function setup() {
     }
 }
 
-function update() {
+var first = true;
+function numAliveNeighbours(x, y) {
+    var numAlive = 0;
+    for (let i = x-1; i < x+2; i++) {
+        for (let j = y-1; j < y+2; j++) {
+            
+            // It it's itself
+            if (i == x && j == y) {
+                continue;
+            }
+            if (i > numCellsX-1 || i < 0 || j > numCellsY-1 ||j < 0) {
+                continue;
+            }
+            if (first) {
+                console.log('i: ' + i + 'j: ' + j + ' | value: ' + previousGeneration[x][y]);
+            }
+            try {
+                if (previousGeneration[i][j] == 1) {
+                    numAlive++;
+                }
+                // numAlive += previousGeneration[x][y];
+            } catch(err){console.log("ERROR");}
+        }
+    }
+    if (first) {
+        console.log('looking at node(x, y): ' + x + ', ' +  y + ' value: ' + previousGeneration[x][y] + ' numAlive = ' + numAlive);
+    }
+    first = false;
+    return numAlive;
+}
+
+function shouldLive(x, y) {
+    // If it's alive at the moment
+    if (previousGeneration[x][y] == 1) {
+        if (numAliveNeighbours(x, y) < 2) {
+            return false;
+        } else if (numAliveNeighbours(x, y) < 4) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (numAliveNeighbours(x, y) == 3) { // It's dead at the moment
+        return true;
+    }
+}
+
+/**
+ * Should work?
+ */
+function copyArray(oldArr) {
+    var newArr = new Array(numCellsX);
     for (let i = 0; i < dataGrid.length; i++) {
-        for (let j = 0; j < dataGrid.length; j++) {
+        newArr[i] = new Array(numCellsY);
+        for (let j = 0; j < dataGrid[i].length; j++) {
+            newArr[i][j] = dataGrid[i][j];
+        }
+    }
+    return newArr;
+}
+
+function update() {
+    previousGeneration = copyArray(dataGrid);
+    for (let i = 0; i < numCellsX; i++) {
+        for (let j = 0; j < numCellsY; j++) {
+            // If the cell should live in the next generation
+            if(shouldLive(i, j)) {
+                dataGrid[i][j] = 1;
+            } else {
+                dataGrid[i][j] = 0;
+            }
         }
     }
 }
@@ -58,19 +126,21 @@ function paint() {
     for (let i = 0; i < dataGrid.length; i++) {
         for (let j = 0; j < dataGrid[i].length; j++) {
             if (dataGrid[i][j] == 1) {
+                c.fillRect(i*cellSize, j*cellSize, cellSize, cellSize);
+            } else {
+                c.clearRect(i*cellSize, j*cellSize, cellSize, cellSize);
             }
         }
     }
 }
 
 function game() {
-    // update();
+    update();
     paint();
 }
 
 setup();
+paint();
 
-let FPS = 30;
+let FPS = 10;
 setInterval(game, 1000/FPS);
-
-
